b96864ca7922319d5a1bb2a190569954b8d92e40
Vishnu <128610832+Vishnuu011@users.noreply.github.com>
2024-03-10 07:44:25 +0000

    data trans update

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from src.mlproject.logger import logging
 from src.mlproject.components.data_ingestion import DataIngestion
+from src.mlproject.components.data_transformation import DataTransformation
 
 logging.info("HI this first log")
 
@@ -7,3 +8,6 @@ if __name__ == '__main__':
 
    data_inges= DataIngestion()
    train_data_path, test_data_path=data_inges.initiate_data_ingestion()
+
+   data_transfom=DataTransformation()
+   data_transfom.initialize_data_transformation(train_data_path, test_data_path)
--- src/mlproject/utils.py
+++ src/mlproject/utils.py
@@ -5,6 +5,19 @@ import pandas as pd
 import numpy as np
 from src.mlproject.logger import logging
 from src.mlproject.exception import CustomException
+import pickle
 
 #def load_dataframe(path:str, file_name:str):
     #return pd.read_csv(Path(os.path.join(path, file_name)))
+
+def save_object(file_path, obj):
+    try:
+        dir_path = os.path.dirname(file_path)
+
+        os.makedirs(dir_path, exist_ok=True)
+
+        with open(file_path, "wb") as file_obj:
+            pickle.dump(obj, file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
